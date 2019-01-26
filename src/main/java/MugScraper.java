e812ea176f11c902d84fd821e888766257c619e8
ccunning2 <cameron.cunning@huskers.unl.edu>
2019-01-26 10:41:57 -0600

    It kindof almost works

--- src/main/java/MugScraper.java
+++ src/main/java/MugScraper.java
@@ -1,36 +1,36 @@
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.*;
-import com.gargoylesoftware.htmlunit.javascript.JavaScriptErrorListener;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.File;
+import java.nio.file.Paths;
 import java.util.List;
 
-import static java.lang.Thread.sleep;
 
 public class MugScraper {
 
+    private static final String PATH_TO_GECKODRIVER = "C:\\geckodriver.exe";
+    private static final String PATH_TO_UBLOCK = "Resources/uBlock0@raymondhill.net.xpi";
+    private static final String DESIRED_CLASS = "mugshots__inmate";
 
     public static void main(String[] args) {
-        try (final WebClient client = new WebClient()) {
-
-
-            //client.getOptions().setCssEnabled(false);
-            //client.getOptions().setJavaScriptEnabled(false);
-
-            final HtmlPage page = client.getPage("https://journalstar.com/mugshots/");
-            sleep(15000);
-            System.out.println(page.getBody().asText());
-           // page.save(new File("/Users/cameroncunning/Java/test"));
-            final DomNodeList<DomNode> divs = page.querySelectorAll("div.mugshots__inmates");
-//            List<HtmlElement> items = (List<HtmlElement>) page.getByXPath("//div[@class='mugshots__inmate']");
-            for (DomNode item : divs) {
-                System.out.println(item.asText());
+        System.setProperty("webdriver.gecko.driver", PATH_TO_GECKODRIVER);
+        FirefoxDriver driver = new FirefoxDriver();
+        //WebDriverWait wait = new WebDriverWait(driver, 10);
+        driver.installExtension(Paths.get(PATH_TO_UBLOCK));
+        try {
+            driver.get("https://www.journalstar.com/mugshots");
+            List<WebElement> mugshots = (new WebDriverWait(driver, 15))
+                    .until(ExpectedConditions.presenceOfAllElementsLocatedBy((By.className(DESIRED_CLASS))));
+            for (WebElement mugshot : mugshots) {
+                System.out.println(mugshot.getText());
             }
+            //sleep(25000);
+        } finally {
 
-//            final HtmlDivision div = page.getHtmlElementById("some_div_id");
-//            final HtmlAnchor anchor = page.getAnchorByName("anchor_name");
-        } catch (Exception e) {
-            e.printStackTrace();
+
+            driver.quit();
         }
     }
 }
